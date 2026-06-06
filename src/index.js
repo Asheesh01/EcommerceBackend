8644f94580e859a52d3c7feab6b6a414d6c1f727
Asheesh01 <ka301423@gmail.com>
2026-06-06 15:10:04 +0530

    add orderController

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const userRoutes = require('./route/userroute')
 const productRoutes=require('./route/producrRoute')
 const cartRoutes=require('./route/cartRoute')
+const oderRoute=require('./route/orderRoute')
 const Connectdb = require('./config/db');
 app.use(express.json());
 dotenv.config();
@@ -12,6 +13,7 @@ const port = process.env.PORT;
 app.use('/api',userRoutes)
 app.use('/api',productRoutes);
 app.use('/api',cartRoutes)
+app.use('/api',oderRoute)
 app.listen(port, () => {
     console.log(`Server is running in the port ${port}`)
 })
